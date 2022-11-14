716e78f698e816aafdb1bceab76f60b7105c7b77
Benry <itoutouberny@gmail.com>
2022-11-14 22:54:15 +0100

    wip-feat(Various): reformat code to add route and retrieve user id

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import './app.scss'
 import React, {useEffect} from 'react';
 import Layout from "./components/Layout/Layout.jsx";
-import Home from "./views/home/Home.jsx";
 import {useFetch} from "./lib/func.js";
+import {Route,Routes} from "react-router-dom";
+import Home from "./views/home/Home.jsx";
 
 const App = () => {
-    const {data: userData, isPending, error} = useFetch('http://localhost:3000/user/12');
-    const {data: userActivity, isPending: activityLoading, error: activityError} = useFetch('http://localhost:3000/user/12/activity');
-    const {data: performanceData, isPending: performanceLoading, error: performanceError} = useFetch('http://localhost:3000/user/12/performance');
-    const {data: sessionsAverageData, isPending: sessionsAverageLoading, error: sessionsAverageError} = useFetch('http://localhost:3000/user/12/average-sessions');
-
     return (
         <Layout>
-            <Home userData={userData} activityData={userActivity} performanceData={performanceData} sessionData={sessionsAverageData}/>
+            <Routes>
+                <Route exact path="/:id" element={<Home />} />
+                <Route exact path="/" element={<Home />} />
+            </Routes>
         </Layout>
     );
 };
